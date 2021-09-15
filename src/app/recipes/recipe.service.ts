0c5e4dd405346eb840c57eb6be45ed76ab584172
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-09-15 21:23:59 +0530

    Include Ingredients in Recipe Model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 
@@ -10,11 +11,21 @@ export class RecipeService {
     new Recipe(
         "Burger", 
         "A hamburger is a food, typically considered a sandwich",
-        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/h3qzjekted0xjdpdrbjz"),
+        "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/h3qzjekted0xjdpdrbjz",
+        [
+          new Ingredient('Bun', 10),
+          new Ingredient('Allo Tikki', 2)
+        ]
+    ),
     new Recipe(
       "Coffee", 
       "Coffee is a brewed drink prepared from roasted coffee beans",
-      "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-2000w,f_auto,q_auto:best/newscms/2019_33/2203981/171026-better-coffee-boost-se-329p.jpg"),
+      "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-2000w,f_auto,q_auto:best/newscms/2019_33/2203981/171026-better-coffee-boost-se-329p.jpg",
+      [
+        new Ingredient('Base', 10),
+        new Ingredient('Cheese', 5)
+      ]
+    ),
   ];
 
   // Event Emitter of selectedRecipe
