493feaf0ddba59abc96a7b763c1a62ab3a65e1b7
Bhavik Vashi <bhavikvashi1804@gmail.com>
2022-03-11 12:39:34 +0530

    DropDown issue fix: close the dropdown if click outside of Dropdown

--- src/app/shared/drop-down.directive.ts
+++ src/app/shared/drop-down.directive.ts
@@ -1,19 +1,26 @@
-import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+} from '@angular/core';
 
 @Directive({
   selector: '[appDropDown]',
 })
 export class DropDownDirective {
-
   @HostBinding('class.open')
   isOpened = false;
 
   constructor(private elementRef: ElementRef) {}
 
-  @HostListener('click')
+  @HostListener('document:click', ['$event'])
   onDropdownClick() {
     //toggle the open
     //this.isOpened =  this.elementRef.nativeElement.contains(event?.target);
-    this.isOpened = !this.isOpened;
+
+    this.isOpened = this.elementRef.nativeElement.contains(event?.target)
+      ? !this.isOpened
+      : false;
   }
 }
