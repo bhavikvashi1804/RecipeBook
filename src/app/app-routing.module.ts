195a7946c80db33a18402ef711382cd4acf5eb3b
Bhavik Vashi <bhavikvashi1804@gmail.com>
2022-03-15 13:04:37 +0530

    Create a new Component for Recipe Edit and setup routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
@@ -9,7 +10,7 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: '/recipes',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'recipes',
@@ -17,22 +18,32 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        component: RecipeStartComponent
+        component: RecipeStartComponent,
       },
+      // first static routes
+      {
+        path: 'new',
+        component: RecipeEditComponent,
+      },
+      // then parameterized route
       {
         path: ':recipeId',
-        component: RecipeDetailComponent
-      }
-    ]
+        component: RecipeDetailComponent,
+      },
+      {
+        path: ':id/edit',
+        component: RecipeEditComponent,
+      },
+    ],
   },
   {
     path: 'shopping-list',
-    component: ShoppingListComponent
-  }
+    component: ShoppingListComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
