6ded7c7f18ab31f9b463a8550617fcfb1d0f955c
Bhavik Vashi <bhavikvashi1804@gmail.com>
2022-03-15 13:39:11 +0530

    New Recipe or Edit Recipe Create Navigation

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.css']
+  styleUrls: ['./recipe-edit.component.css'],
 })
 export class RecipeEditComponent implements OnInit {
+  id!: number;
+  editMode = false;
+  loadedRecipe!: Recipe;
 
-  constructor() { }
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.activateRoute.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      //if ID present then edit else enter new Recipe
+      this.editMode = params['id'] != null;
+      //console.log(this.editMode);
 
+      if (this.editMode) {
+        this.loadedRecipe = this.recipeService.getRecipeById(this.id);
+      }
+    });
+  }
 }
