bb182314827d425106b93d0d53e7decc65925bd5
Bhavik Vashi <bhavikvashi1804@gmail.com>
2022-03-17 11:39:30 +0530

    Shopping Edit Template Driven Form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,9 +1,5 @@
-import {
-  Component,
-  ElementRef,
-  OnInit,
-  ViewChild
-} from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -13,16 +9,14 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput')
-  itemName!: ElementRef;
-
-  @ViewChild('amountInput')
-  itemAmount!: ElementRef;
+  @ViewChild('shoppingForm ')
+  shoppingForm!: NgForm;
 
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit(): void {}
 
+  /*
   onAddItem() {
     let ingredient = new Ingredient(
       this.itemName.nativeElement.value,
@@ -33,5 +27,22 @@ export class ShoppingEditComponent implements OnInit {
 
     this.itemName.nativeElement.value = '';
     this.itemAmount.nativeElement.value = '';
+  }
+  */
+
+  saveDetails() {
+    let ingredient = new Ingredient(
+      this.shoppingForm.value.name,
+      this.shoppingForm.value.amount
+    );
+
+    console.log(ingredient);
+    this.shoppingListService.addNewIngredientToList(ingredient);
+
+    this.shoppingForm.reset();
+  }
+
+  resetShoppingForm(){
+    this.shoppingForm.reset();
   }
 }
