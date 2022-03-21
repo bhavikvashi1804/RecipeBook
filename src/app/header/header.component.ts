4594b5cbd3df9139a5e29fe38416a961315b3488
Bhavik Vashi <bhavikvashi1804@gmail.com>
2022-03-21 14:51:19 +0530

    Store the data to Firebase

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,21 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
   collapsed = true;
-  
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private storageService: DataStorageService) {}
+
+  ngOnInit(): void {}
+
+  onSaveData() {
+    this.storageService.storeRecipes();
   }
 
+  onFetchData() {}
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,10 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataStorageService {
+  constructor(
+    private http: HttpClient,
+    private recipesService: RecipeService
+  ) {}
 
-  constructor(private http: HttpClient) { }
+  storeRecipes() {
+    const recipes = this.recipesService.getRecipes();
+    this.http
+      .put(
+        'https://angular-learn-70c92-default-rtdb.firebaseio.com/data.json',
+        {
+          recipes,
+        }
+      )
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
 }
