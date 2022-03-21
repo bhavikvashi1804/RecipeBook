88f02d670eccb2b7619d3e9fee82556a9b3eb3b6
Bhavik Vashi <bhavikvashi1804@gmail.com>
2022-03-21 20:47:25 +0530

    Create Auth Form

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css'],
+})
+export class AuthComponent implements OnInit {
+  isLoginMode = true;
+
+  @ViewChild('authForm')
+  authForm!: NgForm;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  switchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit() {
+    //console.log(this.authForm.value.email);
+
+    this.authForm.reset();
+  }
+}
