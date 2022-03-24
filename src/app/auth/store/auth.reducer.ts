4b806c3fc50a008bbb1a534719cb4b4822c45764
Bhavik Vashi <bhavikvashi1804@gmail.com>
2022-03-24 19:16:12 +0530

    Creating Auth Actions & Reducers

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,5 +1,6 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
 import { User } from '../user.model';
+import * as AuthActions from './auth.actions';
 
 export interface State {
   user: User | null;
@@ -9,4 +10,19 @@ const initialState: State = {
   user: null,
 };
 
-export const authReducer = createReducer(initialState);
+export const authReducer = createReducer(
+  initialState,
+  on(AuthActions.login, (state, { email, userId, token, expirationDate }) => {
+    let userData = new User(email, userId, token, expirationDate);
+    return {
+      ...state,
+      user: userData,
+    };
+  }),
+  on(AuthActions.logout, (state) => {
+    return {
+      ...state,
+      user: null,
+    };
+  })
+);
